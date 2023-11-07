97d2e1335408b810e3c1a31f80c2644d21dcf36b
Ombuki <125642777+ElvisOmbuki@users.noreply.github.com>
2023-11-07 13:52:50 +0300

    Add files via upload

---
+++ jobsmatdata.js
@@ -0,0 +1,16 @@
+let shopItemsData = [
+    {
+      id: "jfhgbvnscs40",
+      name: "Guest Car per day",
+      price: 10000,
+      desc: "Luxury Land cruser that comes with a driver and fully insured",
+      img: "te1.jpeg",
+    },
+    {
+      id: "ioytrhndcv41",
+      name: "Bus transportation per day",
+      price: 15000,
+      desc: "A minimum of 5 luxury buses for mass transportation",
+      img: "te2.jpeg",
+    },
+]
---
+++ jobsmatic.js
@@ -0,0 +1,76 @@
+let shop = document.getElementById("shop3");
+
+let basket = JSON.parse(localStorage.getItem("data")) || [];
+
+let generateShop = () => {
+  return (shop.innerHTML = shopItemsData
+    .map((x) => {
+      let { id, name, price, desc, img } = x;
+      let search = basket.find((x) => x.id === id) || [];
+      return `
+    <div id=product-id-${id} class="item3">
+        <img width="395 " src=${img} alt="">
+        <div class="details">
+          <h3>${name}</h3>
+          <p>${desc}</p>
+          <div class="price-quantity">
+            <h2>Ksh ${price} </h2>
+            <div class="buttons">
+              <a href="mailto:moscooinnovators@gmail.com" class="nabar__links" id="email-page">ADD</a>
+
+              
+            </div>
+          </div>
+        </div>
+      </div>
+    `;
+    })
+    .join(""));
+};
+
+generateShop();
+
+let increment = (id) => {
+  let selectedItem = id;
+  let search = basket.find((x) => x.id === selectedItem.id);
+
+  if (search === undefined) {
+    basket.push({
+      id: selectedItem.id,
+      item: 1,
+    });
+  } else {
+    search.item += 1;
+  }
+
+  // console.log(basket);
+  update(selectedItem.id);
+  localStorage.setItem("data", JSON.stringify(basket));
+};
+let decrement = (id) => {
+  let selectedItem = id;
+  let search = basket.find((x) => x.id === selectedItem.id);
+
+  if (search === undefined) return;
+  else if (search.item === 0) return;
+  else {
+    search.item -= 1;
+  }
+  update(selectedItem.id);
+  basket = basket.filter((x) => x.item !== 0);
+  // console.log(basket);
+  localStorage.setItem("data", JSON.stringify(basket));
+};
+let update = (id) => {
+  let search = basket.find((x) => x.id === id);
+  // console.log(search.item);
+  document.getElementById(id).innerHTML = search.item;
+  calculation();
+};
+
+let calculation = () => {
+  let cartIcon = document.getElementById("cartAmount");
+  cartIcon.innerHTML = basket.map((x) => x.item).reduce((x, y) => x + y, 0);
+};
+
+calculation();
